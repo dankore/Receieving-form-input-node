3f8b47ac11b21dfb8cbc27964d9fe65386e3339d
Adamu Muhammad Dankore <45286534+dankore@users.noreply.github.com>
2019-08-07 19:50:14 -0500

    Post request body working

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 let express = require("express");
-var bodyParser = require("body-parser");
 
 let myServer = express();
 
-myServer.use(bodyParser.urlencoded({ extended: false }));
-myServer.use(bodyParser.json());
+myServer.use(express.urlencoded({ extended: true }));
 
 myServer.get("/", (req, res) => {
   res.send(`<!DOCTYPE html>
@@ -18,8 +16,8 @@ myServer.get("/", (req, res) => {
     <div class="m-32 md:w-1/2 md:m-auto md:mt-32">
     <h1 class="text-center my-6 text-2xl"> What is the color of grass?</h1>
     <form method="POST" action="/answer" class="outline-0 border border-transparent border-grey-400 flex">
-    <input nameColor="green" autocomplete="off" class="border shadows:inset px-3 rounded w-full py-2" type="text" placeholder="Type here">
-      <button class="px-4 ml-2 rounded py-2 bg-green-500 hover:bg-green-700 text-white font-bold shadow-lg">Submit</button>
+    <input name=color autocomplete="off" class="border shadows:inset px-3 rounded w-full py-2" type="text" placeholder="Type here">
+      <input type="submit" value="Submit" class="px-4 ml-2 rounded py-2 bg-green-500 hover:bg-green-700 text-white font-bold shadow-lg" />
     </form>
     </div>
     </body>
@@ -29,15 +27,15 @@ myServer.get("/", (req, res) => {
 
 myServer.post("/answer", (req, res) => {
   console.log(req.body);
-  if (req.body.nameColor === "green") {
+  if (req.body.color === "green") {
     res.send("You are right!");
   } else {
     res.send("Sorry, not the correct answer");
   }
 });
 
-myServer.get("/answer", (req, res) => {
-  res.send("Are you lost or something?");
-});
+// myServer.get("/answer", (req, res) => {
+//   res.send("Are you lost or something?");
+// });
 
 myServer.listen(3000);
